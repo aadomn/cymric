88b3f478d5cef8bd0a81a44c9bab04e5454daa8d
Alexandre Adomnicăi <alexandre@adomnicai.me>
2025-04-04 18:09:35 +0200

    Remove unnecessary variable

--- src/cymric-aes128/x86_64/test/main.c
+++ src/cymric-aes128/x86_64/test/main.c
@@ -10,9 +10,7 @@ int main(void) {
                              0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
     uint8_t ptext[16]     = {0x7f, 0x43, 0xf6, 0xaf, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34};
     uint8_t ctext[32]     = {0x00};
-    uint8_t ptext_bis[16] = {0x00};
     size_t outlen;
-
 
     cipher_ctx_t aes_ctx = aes_get_cipher_ctx();
     aes_roundkeys_t aes_keys;
@@ -23,10 +21,10 @@ int main(void) {
     for(size_t i = 0; i < outlen; i++)
       printf("%02x", ctext[i]);
     printf("\n");
-    ret = cymric1_dec(ptext_bis, &outlen, key, nonce, 12, ctext, outlen, ad, 3, &aes_ctx);
+    ret = cymric1_dec(ptext, &outlen, key, nonce, 12, ctext, outlen, ad, 3, &aes_ctx);
     printf("manx1_dec (12, 4, 3) returned %d and outlen = %ld\n", ret, outlen);
     for(size_t i = 0; i < outlen; i++)
-      printf("%02x", ptext_bis[i]);
+      printf("%02x", ptext[i]);
     printf("\n");
 
     ret = cymric1_enc(ctext, &outlen, key, nonce, 8, ptext, 8, ad, 4, &aes_ctx);
@@ -34,10 +32,10 @@ int main(void) {
     for(size_t i = 0; i < outlen; i++)
       printf("%02x", ctext[i]);
     printf("\n");
-    ret = cymric1_dec(ptext_bis, &outlen, key, nonce, 8, ctext, outlen, ad, 4, &aes_ctx);
+    ret = cymric1_dec(ptext, &outlen, key, nonce, 8, ctext, outlen, ad, 4, &aes_ctx);
     printf("manx1_dec (8, 8, 4) returned %d and outlen = %ld\n", ret, outlen);
     for(size_t i = 0; i < outlen; i++)
-      printf("%02x", ptext_bis[i]);
+      printf("%02x", ptext[i]);
     printf("\n");
 
     ret = cymric2_enc(ctext, &outlen, key, nonce, 12, ptext, 16, ad, 3, &aes_ctx);
@@ -45,9 +43,11 @@ int main(void) {
     for(size_t i = 0; i < outlen; i++)
       printf("%02x", ctext[i]);
     printf("\n");
-    ret = cymric2_dec(ptext_bis, &outlen, key, nonce, 12, ctext, outlen, ad, 3, &aes_ctx);
+    ret = cymric2_dec(ptext, &outlen, key, nonce, 12, ctext, outlen, ad, 3, &aes_ctx);
     printf("manx2_dec (12, 16, 3) returned %d and outlen = %ld\n", ret, outlen);
     for(size_t i = 0; i < outlen; i++)
-      printf("%02x", ptext_bis[i]);
+      printf("%02x", ptext[i]);
     printf("\n");
+
+    return 0;
 }
